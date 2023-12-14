42837859b2b24120b518a3d8628a9a6d07f77ec1
Prãt!k Kàbádé <pratikkabade@hotmail.com>
2023-12-14 14:00:00 +0100

    Add function to convert Android `colorInt` to `hex`

--- src/hooks/Functions.tsx
+++ src/hooks/Functions.tsx
@@ -8,11 +8,30 @@ export function getDate(date: any) {
     const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
     return `${months[m.getMonth()]} ${m.getDate()}`;
 }
-export function getYear(date: any){
+export function getYear(date: any) {
     const y = new Date(date);
-    return y.toISOString().slice(0,4)
+    return y.toISOString().slice(0, 4)
 }
-export function getMonth(date: any){
+export function getMonth(date: any) {
     const y = new Date(date);
-    return y.toISOString().slice(5,7)
+    return y.toISOString().slice(5, 7)
+}
+export function androidColorToHex(androidColor: any) {
+    // Ensure the input is treated as an unsigned 32-bit integer
+    let color = androidColor >>> 0;
+
+    // Extract ARGB components
+    let alpha = (color >> 24) & 0xFF;
+    let red = (color >> 16) & 0xFF;
+    let green = (color >> 8) & 0xFF;
+    let blue = color & 0xFF;
+
+    // Convert to hex string and pad with zeros if necessary
+    let hexColor = "#" +
+        alpha.toString(16).padStart(2, '0') +
+        red.toString(16).padStart(2, '0') +
+        green.toString(16).padStart(2, '0') +
+        blue.toString(16).padStart(2, '0');
+
+    return hexColor.toUpperCase(); // Return the hex color code
 }
