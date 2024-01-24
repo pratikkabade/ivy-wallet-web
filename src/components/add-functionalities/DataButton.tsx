605e3e93a546feedbf563eee25ddec05eedcdd25
Prãt!k Kàbádé <pratikkabade@hotmail.com>
2024-01-24 14:00:00 +0100

    Refactor DataButton component to improve button styling and functionality

--- src/components/add-functionalities/DataButton.tsx
+++ src/components/add-functionalities/DataButton.tsx
@@ -23,8 +23,12 @@ export const DataButton = () => {
                 <Modal.Header>Add the URL</Modal.Header>
                 <Modal.Body className="slide-up">
                     <JSONdata />
-                    <div className="flex justify-end">
-                        <Button onClick={clearStorage} className="mt-5 bg-red-500 hover:bg-red-700 dark:bg-red-500 dark:hover:bg-red-700"> Remove </Button>
+                    <div className="flex justify-between mt-5">
+
+                        <button onClick={clearStorage} type="button" className="focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-900">Remove</button>
+
+                        <button onClick={() => setOpenModal('false')} type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Save</button>
+
                     </div>
                 </Modal.Body>
             </Modal>
